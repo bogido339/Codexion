15ac678c4987d49bdb16d7e48ca99474f5371527
Mohamed Bougajdi <mbougajd@c3r8p2.1337.ma>
2026-04-24 18:29:18 +0100

    fix did lock

--- coders/heap_manager.c
+++ coders/heap_manager.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heap_manager.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mohamed <mohamed@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mbougajd <mbougajd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 16:51:50 by mbougajd          #+#    #+#             */
-/*   Updated: 2026/04/24 16:09:16 by mohamed          ###   ########.fr       */
+/*   Updated: 2026/04/24 18:25:11 by mbougajd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,71 +14,59 @@
 
 void add_coder_to_heap(t_coder *coder)
 {
-    pthread_mutex_lock(&coder->left_dongle->heap_mutex);
-    if (!coder->left_dongle->heap[0])
-        coder->left_dongle->heap[0] = coder;
+    if (!get_heap(coder->left_dongle, 0))
+        set_heap(coder->left_dongle, coder, 0);
     else
-        coder->left_dongle->heap[1] = coder;
-    pthread_mutex_unlock(&coder->left_dongle->heap_mutex);
+        set_heap(coder->left_dongle, coder, 1);
 
-    pthread_mutex_lock(&coder->right_dongle->heap_mutex);
-    if (!coder->right_dongle->heap[0])
-        coder->right_dongle->heap[0] = coder;
+    if (!get_heap(coder->right_dongle, 0))
+        set_heap(coder->right_dongle, coder, 0);
     else
-        coder->right_dongle->heap[1] = coder;
-    pthread_mutex_unlock(&coder->right_dongle->heap_mutex);
+        set_heap(coder->right_dongle, coder, 1);
 }
 
 void enqueue_first_time(t_coder *coder)
 {
-    pthread_mutex_lock(&coder->left_dongle->heap_mutex);
-    pthread_mutex_lock(&coder->right_dongle->heap_mutex);
-    if (coder->id % 2 != 0 && !coder->left_dongle->heap[0] && !coder->right_dongle->heap[0])
+    if (coder->id % 2 != 0 && !get_heap(coder->left_dongle, 0) && !get_heap(coder->right_dongle, 0))
     {
-        coder->left_dongle->heap[0] = coder;
-        coder->right_dongle->heap[0] = coder;
+        set_heap(coder->left_dongle, coder, 0);
+        set_heap(coder->right_dongle, coder, 0);
     }
-    else if (coder->id % 2 == 0 && !coder->left_dongle->heap[1] && !coder->right_dongle->heap[1])
+    else if (coder->id % 2 == 0 && !get_heap(coder->left_dongle, 1) && !get_heap(coder->right_dongle, 1))
     {
-        coder->left_dongle->heap[1] = coder;
-        coder->right_dongle->heap[1] = coder;
+        set_heap(coder->left_dongle, coder, 1);
+        set_heap(coder->right_dongle, coder, 1);
     }
-    else if (!coder->left_dongle->heap[1] && coder->right_dongle->heap[1])
+    else if (!get_heap(coder->left_dongle, 1) && get_heap(coder->right_dongle, 1))
     {
-        coder->left_dongle->heap[1] = coder;
-        coder->right_dongle->heap[0] = coder;
+        set_heap(coder->left_dongle, coder, 1);
+        set_heap(coder->right_dongle, coder, 0);
     }
     else
     {
-        coder->left_dongle->heap[0] = coder;
-        coder->right_dongle->heap[1] = coder;
+        set_heap(coder->left_dongle, coder, 0);
+        set_heap(coder->right_dongle, coder, 1);
     }
     if (coder->first_push)
-        coder->first_push = 0;
-    pthread_mutex_unlock(&coder->right_dongle->heap_mutex);
-    pthread_mutex_unlock(&coder->left_dongle->heap_mutex);   
+        coder->first_push = 0;   
 }
 
 void dequeue_from_heap(t_coder *coder)
 {
-    pthread_mutex_lock(&coder->left_dongle->heap_mutex);
-    coder->left_dongle->heap[0] = coder->left_dongle->heap[1];
-    coder->left_dongle->heap[1] = NULL;
-    pthread_mutex_unlock(&coder->left_dongle->heap_mutex);
-    
-    pthread_mutex_lock(&coder->right_dongle->heap_mutex);
-    coder->right_dongle->heap[0] = coder->right_dongle->heap[1];
-    coder->right_dongle->heap[1] = NULL;
-    pthread_mutex_unlock(&coder->right_dongle->heap_mutex);
+    set_heap(coder->left_dongle, get_heap(coder->left_dongle, 1), 0);
+    set_heap(coder->left_dongle, NULL, 1);
+
+    set_heap(coder->right_dongle, get_heap(coder->right_dongle, 1), 0);
+    set_heap(coder->right_dongle, NULL, 1);
 }
 
-void swap_coders(t_coder **heap)
+void swap_coders(t_dongle *dongle)
 {
     t_coder *coder;
     
-    coder = heap[0];
-    heap[0] = heap[1];
-    heap[1] = coder;
+    coder = get_heap(dongle, 0);
+    set_heap(dongle, get_heap(dongle, 1), 0);
+    set_heap(dongle, coder, 1);
 }
 
 void sort_dongle_heap(t_dongle *dongle)
@@ -86,14 +74,14 @@ void sort_dongle_heap(t_dongle *dongle)
     t_coder *c0;
     t_coder *c1;
 
-    if (!dongle->heap[0] || !dongle->heap[1])
+    if (!get_heap(dongle, 0) || !get_heap(dongle, 1))
         return;
         
-    c0 = dongle->heap[0];
-    c1 = dongle->heap[1];
+    c0 = get_heap(dongle, 0);
+    c1 = get_heap(dongle, 1);
 
     if (get_compile_time(c0) > get_compile_time(c1))
-        swap_coders(dongle->heap);
+        swap_coders(dongle);
     else if (get_compile_time(c0) == get_compile_time(c1) && get_compile_count(c0) < get_compile_count(c1))
-        swap_coders(dongle->heap);
+        swap_coders(dongle);
 }
--- coders/scheduler.c
+++ coders/scheduler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   scheduler.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mohamed <mohamed@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mbougajd <mbougajd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 16:51:50 by mbougajd          #+#    #+#             */
-/*   Updated: 2026/04/24 16:08:27 by mohamed          ###   ########.fr       */
+/*   Updated: 2026/04/24 18:03:32 by mbougajd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int schedule_fifo(t_coder *coder)
 {
-    if (coder != coder->left_dongle->heap[0] || coder != coder->right_dongle->heap[0])
+    if (coder != get_heap(coder->left_dongle, 0) || coder != get_heap(coder->right_dongle, 0))
         return (0);
     else
         return (1);
@@ -22,41 +22,22 @@ int schedule_fifo(t_coder *coder)
 
 int schedule_edf(t_coder *coder)
 {
-    if (coder->left_dongle->heap[1] == coder)
+    if (get_heap(coder->left_dongle, 1) == coder)
         sort_dongle_heap(coder->left_dongle);
-    if (coder->right_dongle->heap[1] == coder)
+    if (get_heap(coder->right_dongle, 1) == coder)
         sort_dongle_heap(coder->right_dongle);
 
-    if (coder == coder->left_dongle->heap[0] && coder == coder->right_dongle->heap[0])
+    if (coder == get_heap(coder->left_dongle, 0)
+    && coder == get_heap(coder->right_dongle, 0))
         return (1);
     return (0);
 }
 
 int has_scheduler_turn(t_coder *coder)
 {
-    int result;
-
-    result = 0;
     if (strcmp(coder->config->scheduler, "edf") == 0)
-    {
-        pthread_mutex_lock(&coder->left_dongle->heap_mutex);
-        pthread_mutex_lock(&coder->right_dongle->heap_mutex);
-        result = schedule_edf(coder);
-        pthread_mutex_unlock(&coder->right_dongle->heap_mutex);
-        pthread_mutex_unlock(&coder->left_dongle->heap_mutex);
-        
-        return result;
-    }
-    if (strcmp(coder->config->scheduler, "fifo") == 0)
-    {
-        pthread_mutex_lock(&coder->left_dongle->heap_mutex);
-        pthread_mutex_lock(&coder->right_dongle->heap_mutex);
-        result = schedule_fifo(coder);
-        pthread_mutex_unlock(&coder->right_dongle->heap_mutex);
-        pthread_mutex_unlock(&coder->left_dongle->heap_mutex);
-        
-        return result;
-    }
-    return (result);
+        return schedule_edf(coder);
+    else
+        return schedule_fifo(coder);
 }
 
